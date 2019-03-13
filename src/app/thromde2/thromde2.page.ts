9c492421e23a60eba79414c52515dbf9017fc8ad
tsheling1993 <tashioongchuk@gmail.com>
2019-03-13 18:04:17 +0600

    iiijj

--- src/app/thromde2/thromde2.page.ts
+++ src/app/thromde2/thromde2.page.ts
@@ -15,14 +15,8 @@ export class Thromde2Page implements OnInit {
   showGarbageTruck_var: boolean = false;
   showCitybus_var: boolean = true;
 
-  citybusScheduleColor: string = "tertiary";
-  citybusTrackerColor: string = "medium";
-
   citybusSelectColor: string = "tertiary";
   garbageSelectColor: string = "medium";
-
-  garbageScheduleColor: string = "tertiary";
-  garbageTrackerColor: string = "medium";
 
   showGarbageTracker_var: boolean = false;
   showGarbageSchedule_var: boolean = true;
@@ -51,6 +45,7 @@ export class Thromde2Page implements OnInit {
   ) 
   { 
     this.loadCitybusSchedule();
+    this.loadGarbageSchedule();
   }
 
   ngOnInit() {
@@ -67,19 +62,6 @@ export class Thromde2Page implements OnInit {
   };
 
   //Below codes are all for show and hide features
-  showCitybusTracker(){
-    this.showCitybusTracker_var = true;
-    this.showCitybusSchedule_var = false;
-    this.citybusScheduleColor = "medium";
-    this.citybusTrackerColor = "tertiary";
-  }
-
-  showCitybusSchedule(){
-    this.showCitybusSchedule_var = true;    
-    this.showCitybusTracker_var = false;
-    this.citybusScheduleColor = "tertiary";
-    this.citybusTrackerColor = "medium";
-  }
 
   showGarbageTruck(){
     this.showGarbageTruck_var = true;
@@ -95,18 +77,9 @@ export class Thromde2Page implements OnInit {
     this.garbageSelectColor = "medium";
   }
 
-  showGarbageSchedule(){
-    this.showGarbageSchedule_var = true;    
-    this.showGarbageTracker_var = false;
-    this.garbageScheduleColor = "tertiary";
-    this.garbageTrackerColor = "medium";
-  }
-
   showGarbageTracker(){
     this.showGarbageTracker_var = true;
     this.showGarbageSchedule_var = false;
-    this.garbageScheduleColor = "medium";
-    this.garbageTrackerColor = "tertiary";
   }
 
   changedDaySelectedCity(){
@@ -117,6 +90,7 @@ export class Thromde2Page implements OnInit {
   terminalCity: any;
   daySelectedCity: any;
   destinationCity: any;
+  
 
   changedTerminalSelectedCity(){
     if(this.terminalCity != null){
@@ -156,70 +130,96 @@ export class Thromde2Page implements OnInit {
 
   findTiming(){
     this.showTimeStatus = true;
-    // if()
-  //   terminalCity: any;
-  // daySelectedCity: any;
-  // destinationCity: any;
+  
     if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "changlamCity" && this.destinationCity == "dechencholing"){
       this.timeCitybus = this.mon_chang_dechencholing;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "changlamCity" && this.destinationCity == "babesaViaLungtenphu"){
       this.timeCitybus = this.mon_chang_babesaViaLungtenphu;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "changlamCity" && this.destinationCity == "olakhababesaEW"){
       this.timeCitybus = this.mon_chang_olakBabesaEW;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "changlamCity" && this.destinationCity == "olakhaViaMC"){
       this.timeCitybus = this.mon_chang_olakViaMC;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "changlamCity" && this.destinationCity == "changjijiViaMC"){
       this.timeCitybus = this.mon_chang_changjiViaMC;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "changlamCity" && this.destinationCity == "jungshinaPamtsho"){
       this.timeCitybus = this.mon_chang_junshiPamtsho;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "changlamCity" && this.destinationCity == "motithang"){
       this.timeCitybus = this.mon_chang_motithang;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "chubachuCity" && this.destinationCity == "hontshoMonFri"){
       this.timeCitybus = this.mon_chuba_hontshoWeekdays;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "chubachuCity" && this.destinationCity == "hontshoSat"){
       this.timeCitybus = this.mon_chuba_hontshoSat;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "chubachuCity" && this.destinationCity == "chamgangMonFri"){
       this.timeCitybus = this.mon_chuba_chamgangWeekdays;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "chubachuCity" && this.destinationCity == "chamgangSat"){
       this.timeCitybus = this.mon_chuba_chamgangSat;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "chubachuCity" && this.destinationCity == "kuenselphodrang"){
       this.timeCitybus = this.mon_chuba_kuenselphodrang;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCityMonSat" && this.terminalCity == "chubachuCity" && this.destinationCity == "serbithang"){
       this.timeCitybus = this.mon_chuba_serbithang;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCitySun" && this.terminalCity == "changlamCity" && this.destinationCity == "dechencholing"){
       this.timeCitybus = this.sun_chang_dechencholing;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCitySun" && this.terminalCity == "changlamCity" && this.destinationCity == "babesaViaLungtenphu"){
       this.timeCitybus = this.sun_chang_babesaViaLungtenphu;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCitySun" && this.terminalCity == "changlamCity" && this.destinationCity == "olakhababesaEW"){
       this.timeCitybus = this.sun_chang_olakBabesaEW;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCitySun" && this.terminalCity == "changlamCity" && this.destinationCity == "jungshinaPamtsho"){
       this.timeCitybus = this.sun_chang_junshiPamtsho;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCitySun" && this.terminalCity == "changlamCity" && this.destinationCity == "motithang"){
       this.timeCitybus = this.sun_chang_motithang;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCitySun" && this.terminalCity == "chubachuCity" && this.destinationCity == "hontshoSun"){
       this.timeCitybus = this.sun_chuba_hontsho;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
     else if(this.daySelectedCity == "dayCitySun" && this.terminalCity == "chubachuCity" && this.destinationCity == "chamgangSun"){
       this.timeCitybus = this.sun_chuba_chamgang;
+      this.showSplittedCityBusResult(this.timeCitybus);
     }
+  }
+
+  morningCitySchedule: any;
+  lunchCitySchedule: any;
+
+  showSplittedCityBusResult(timeCitybus: any){
+    let splitResult = timeCitybus.split("|");
+    this.morningCitySchedule = splitResult[0];
+    this.lunchCitySchedule = splitResult[1];
   }
 
   mon_chang_babesaViaLungtenphu: any;
@@ -308,4 +308,211 @@ export class Thromde2Page implements OnInit {
   goGarbageTruck(){
     this.navCtl.navigateForward('/trackgarbagetruck');
   }
+
+  //Garbage Schedule
+  zoneSelected: any;
+  garbageLocation: any;
+  locationButton: boolean = true;
+
+  garbageScheduleData:any[]=[];
+
+  centralZone_var: boolean = true;
+  southZone_var: boolean = false;
+  northZone_var: boolean = false;
+
+  garbageButton: boolean = true;
+  garbageResult: any;
+
+  showGarbageResult: boolean = false;
+
+  changedZone(){
+    if(this.zoneSelected != null){
+      this.locationButton = false;
+    }
+    if(this.zoneSelected == "central"){
+      this.centralZone_var = true;
+      this.southZone_var = false;
+      this.northZone_var = false;
+    }
+    else if(this.zoneSelected == "south"){
+      this.southZone_var = true;
+      this.centralZone_var = false;
+      this.northZone_var = false;
+    }
+    else if(this.zoneSelected == "north"){
+      this.northZone_var = true;
+      this.centralZone_var = false;
+      this.southZone_var = false;
+    }
+
+  }
+
+  changedLocation(){
+    if(this.garbageLocation != null){
+      this.garbageButton = false;
+    }
+  }
+
+  firstResultGarbage : any;
+  secondResultGarbage : any;
+
+  findGarbageSchedule(){
+    this.showGarbageResult = true;
+    if(this.garbageLocation == "upperMotithang"){
+      this.garbageResult = this.upperMotithang;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "lowerMotithang"){
+      this.garbageResult = this.lowerMotithang;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "changangkha"){
+      this.garbageResult = this.changangkha;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "nppfColony"){
+      this.garbageResult = this.nppfColony;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "nordzinAndHongkongMkt"){
+      this.garbageResult = this.nordzinLamAndHongMkt;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "nordzinAndPwd"){
+      this.garbageResult = this.nordzinLamAndPWD;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "changlamAndCFM"){
+      this.garbageResult = this.changlamAndCFM;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "changlamAndChubachu"){
+      this.garbageResult = this.changlamAndChubachu;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "changedaphu"){
+      this.garbageResult = this.changedaphu;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "changbangdu"){
+      this.garbageResult = this.changbangdu;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "policeHospital"){
+      this.garbageResult = this.policeCamp;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "changzamtog"){
+      this.garbageResult = this.changzamtog;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "changjiji"){
+      this.garbageResult = this.changjiji;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "olakha"){
+      this.garbageResult = this.olakha;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "lungtenphu"){
+      this.garbageResult = this.lungtenphu;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "semtokha"){
+      this.garbageResult = this.semtokha;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "babesaGref"){
+      this.garbageResult = this.babesaGref;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "yhssBabesa"){
+      this.garbageResult = this.YhssBabesa;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "babesaNew"){
+      this.garbageResult = this.babesaNew;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+    else if(this.garbageLocation == "babesaOld"){
+      this.garbageResult = this.babesaOld;
+      this.showSplittedGarbageResult(this.garbageResult);
+    }
+  }
+
+  showSplittedGarbageResult(garbageResult: any){
+    let splitResult = garbageResult.split("|");
+    this.firstResultGarbage = splitResult[0];
+    this.secondResultGarbage = splitResult[1];
+  }
+
+  upperMotithang: any;
+  lowerMotithang: any;
+  changangkha: any;
+  nppfColony: any;
+  nordzinLamAndHongMkt: any;
+  nordzinLamAndPWD: any;
+  changlamAndCFM: any;
+  changlamAndChubachu: any;
+  changedaphu: any;
+  changbangdu: any;
+  policeCamp: any;
+  changzamtog: any;
+  changjiji: any;
+  olakha: any;
+  lungtenphu: any;
+  semtokha: any;
+  babesaGref: any;
+  YhssBabesa: any;
+  babesaNew: any;
+  babesaOld: any;
+
+  loadGarbageSchedule(){
+    this.fs.collection('/thromde').doc('garbageTruckSchedule').get().subscribe(res=>  
+      {
+        this.garbageScheduleData.push({
+          upperMotithang:res.data().upperMotithang,
+          lowerMotithang:res.data().lowerMotithang,
+          changangkha : res.data().changangkha,
+          nppfColony :res.data().nppfColony,
+          nordzinLamAndHongMkt : res.data().hongkongMarket,
+          nordzinLamAndPWD : res.data().pwdColony,
+          changlamAndCFM : res.data().cfmResident,
+          changlamAndChubachu : res.data().chubachu,
+          changedaphu : res.data().changedaphu,
+          changbangdu : res.data().changbangdu,
+          policeCamp : res.data().policecamp,
+          changzamtog : res.data().changzamtog,
+          changjiji : res.data().changjiji,
+          olakha : res.data().olakha,
+          lungtenphu : res.data().lungtenphu,
+          semtokha : res.data().semtokha,
+          babesaGref : res.data().babesaGref,
+          YhssBabesa : res.data().YHS_Babesa,
+          babesaNew : res.data().babesaNewlab,
+          babesaOld : res.data().babesaOldLab
+        })
+        this.upperMotithang = res.data().upperMotithang;
+        this.lowerMotithang = res.data().lowerMotithang,
+        this.changangkha = res.data().changangkha,
+        this.nppfColony = res.data().nppfColony,
+        this.nordzinLamAndHongMkt = res.data().hongkongMarket,
+        this.nordzinLamAndPWD = res.data().pwdColony,
+        this.changlamAndCFM = res.data().cfmResident,
+        this.changlamAndChubachu = res.data().chubachu,
+        this.changedaphu = res.data().changedaphu,
+        this.changbangdu = res.data().changbangdu,
+        this.policeCamp = res.data().policecamp,
+        this.changzamtog = res.data().changzamtog,
+        this.changjiji = res.data().changjiji,
+        this.olakha = res.data().olakha,
+        this.lungtenphu = res.data().lungtenphu,
+        this.semtokha = res.data().semtokha,
+        this.babesaGref = res.data().babesaGref,
+        this.YhssBabesa = res.data().YHS_Babesa,
+        this.babesaNew = res.data().babesaNewlab,
+        this.babesaOld = res.data().babesaOldLab
+      })
+      console.log("Garbage Schedule"+this.garbageScheduleData);
+    }
 }
